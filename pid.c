220823b21639d8f0bbfaafc00d222744161a1a1b
Eliot Partridge <bytewave@ponypwnage.com>
2017-11-27 12:15:30 -0600

    Fix PID formatting and syntax

--- pid.c
+++ pid.c
@@ -1,23 +1,23 @@
-#define kP=1
-#define kD=10
-double lastPosition=0;
-double pid (double setpoint, double position){
+#define kP 1
+#define kD 10
+
+double lastPosition = 0;
+
+double pid(double setpoint, double position) {
 	double error = setpoint - position;
 
-	double prop= kP*error;
-	double deriv= kD*(position-lastPosition);
+	double prop = kP * error;
+	double deriv = kD * (position - lastPosition);
 	//check if first pass through
-	if (!floor(lastPosition)){
-		deriv=0;
+	if (!floor(lastPosition)) {
+		deriv = 0;
 	}
-	return prop-deriv;
+	return prop - deriv;
 
 }
 
-int[] drive(int speed, double pos, double setpoint){
-
+int *drive(int speed, double pos, double setpoint){
 	int turn = pid(setpoint, pos);
-	int[2] speeds= {speed+turn, speed-turn};
+	int speeds[2] = {speed + turn, speed - turn};
 	return speeds;
-
 }
